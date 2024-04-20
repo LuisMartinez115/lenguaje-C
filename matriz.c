72a73ee7341d72d193cf984f13c1b4bc2ba02d3a
Luis_Martinez115 <101226571+LuisMartinez115@users.noreply.github.com>
2024-04-20 14:38:19 -0600

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -1,153 +1,113 @@
 #include <stdio.h>
 #define fore(i, l, r) for (int(i) = (l); (i) < (r); (i)++)
-#define debug(x) printf(#x ": %d\n", x)
 
-
-int main() {
-  while(1){
-
-    char op;
-    printf("escribe el signo de la operación a realizar \n + para suma \n - para resta\n * para multiplicación\n / para division \n t para traspuesta \n x para salir\n");
-    scanf(" %c",&op);
-    if (op=='x')
-      return 0;
-    if (op != 't' && op != 'x')
-    {
-      int m1,m2,n1,n2;
-      //recolectar matriz A
-      printf("ingresa el tamaño de tu matriz A \n  ejemplo: 3 2\n");
-      scanf("%d %d",&m1,&n1);
-
-      int a[m1][n1];
-      fore(i,0,m1)
-      {
-      fore(j,0,n1)
-      {
-      scanf("%d",&a[i][j]);
-      }
-      }
-      //recolectar matriz B
-      printf("ingresa el tamaño de tu matriz B, como lo hiciste con A \n");
-      scanf("%d %d",&m2,&n2);
-      int b[m2][n2];
-      fore(i,0,m2)
-      {
-      fore(j,0,n2)
-      {
-      scanf("%d",&b[i][j]);
-      }
-      }
-      // opciones 
-      switch (op)
-      {
-        case '+':	
-
-          if (m1 ==m2 && n1==n2)
-          {
-            printf("la matriz resultante es \n");
-            fore (i,0,m1)
-            {
-              fore(j,0,n1)
-                {
-                printf("%d ",(a[i][j]+b[i][j]));
-                }
-              printf("\n");
+void leer_mtrz(int m, int n, int matrix[m][n]) {
+    fore(i, 0, m) {
+        fore(j, 0, n) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+void suma_mtrz(int m1, int n1, int a[m1][n1], int m2, int n2, int b[m2][n2]) {
+    if (m1 == m2 && n1 == n2) {
+        fore(i, 0, m1) {
+            fore(j, 0, n1) {
+                printf("%d ", a[i][j] + b[i][j]);
             }
-          }
+            printf("\n");
+        }
+    } else {
+        printf("las matrices no son de las mismas dimensiones\n");
+    }
+}
 
-          else
-          {
-            printf("las matrices no son de las mismas dimensiones\n");
-          }
-          break;
-        case '-':
-
-          if (m1==m2 && n1==n2)
-          {
-            printf("la matriz resultante es \n");
-            fore (i,0,m1)
-            {
-              fore(j,0,n1)
-              {
-                printf("%d ",(a[i][j]-b[i][j]));
-              }
-              printf("\n");
+void resta_mtrz(int m1, int n1, int a[m1][n1], int m2, int n2, int b[m2][n2]) {
+    if (m1 == m2 && n1 == n2) {
+        fore(i, 0, m1) {
+            fore(j, 0, n1) {
+                printf("%d ", a[i][j] - b[i][j]);
             }
-          }
+            printf("\n");
+        }
+    } else {
+        printf("las matrices no son de las mismas dimensiones\n");
+    }
+}
 
-          else
-          {
-            printf("las matrices no son de las mismas dimensiones");
-          }
-          break;
-        case '*':
-
-          if (n1==m2)
-          {
-            printf("la matriz resultante es \n");
-            fore (i,0,m2)
-            {
-              fore(j,0,n1)
-              {
-
-                int r=0;
-                fore(l,0,m2)
-                {
-                  r+=a[i][l]*b[l][j];		
+void mult_matriz(int m1, int n1, int a[m1][n1], int m2, int n2, int b[m2][n2]) {
+    if (n1 == m2) {
+        fore(i, 0, m2) {
+            fore(j, 0, n1) {
+                int r = 0;
+                fore(l, 0, m2) {
+                    r += a[i][l] * b[l][j];
                 }
                 printf("%d ", r);
-              }
-
-              printf("\n");
             }
-          }
-
-          else
-          {
-            printf("no es posible con esas dimensiones, las filas de A deben ser igual a las columnas de B\n");
-          }
-          break;
-        case '/':
-
-          if (m2==n2)
-          {
-              //calcular determinante
-
-              //fin de determinante
-              printf("la matriz resultante es \n");
-          }
-          else
-          {
-              printf("las matrices no son de las mismas dimensiones\n");
-          break;
-          }
-        default:
-        printf("no es una opción válida\n");
-      }
+            printf("\n");
+        }
+    } else {
+        printf("no es posible con esas dimensiones, las filas de A deben ser igual a las columnas de B\n");
     }
-    else{
-      int m,n;
-      //recolectar matriz A
-        printf("ingresa el tamaño de tu matriz  \n  ejemplo: 3 2\n");
-        scanf("%d %d",&m,&n);
-
-        int a[m][n];
-        fore(i,0,m)
-        {
-        fore(j,0,n)
-        {
-        scanf("%d",&a[i][j]);
-        }
-        }
-        printf("la matriz resultante es \n");
-      fore(i,0,n)
-      {
-        fore(j,0,m)
-        {
-          printf("%d ",a[j][i]);
-        }
-        printf("\n");
-      }
+}
+void div_matriz(int m1, int n1, int a[m1][n1], int m2, int n2, int b[m2][n2]){
+  //multiplicacion de matriz por inversa de otra matriz
+}
+void traspuesta(int m, int n, int a[m][n]){
+  printf("la matriz resultante es \n");
+  fore(i,0,n)
+  {
+    fore(j,0,m)
+    {
+      printf("%d ",a[j][i]);
     }
+    printf("\n");
   }
+}
+
+int main() {
+    while (1) {
+        char op;
+        printf("escribe el signo de la operación a realizar\n + para suma\n - para resta\n * para multiplicación\n / para división\n t para traspuesta\n x para salir\n");
+        scanf(" %c", &op);
+      
+        int m1, n1, m2, n2;
+        printf("ingresa el tamaño de tu matriz A\n ejemplo: 3 2\n");
+        scanf("%d %d", &m1, &n1);
+
+        int a[m1][n1];
+        leer_mtrz(m1, n1, a);
+
+        int b[0][0];
+        if (op != 't') {
+            printf("ingresa el tamaño de tu matriz B, como lo hiciste con A\n");
+            scanf("%d %d", &m2, &n2);
+            b[m2][n2];
+            leer_mtrz(m2, n2, b);
+        }
+
+        switch (op) {
+            case '+':
+                suma_mtrz(m1, n1, a, m2, n2, b);
+                break;
+            case '-':
+                resta_mtrz(m1, n1, a, m2, n2, b);
+                break;
+            case '*':
+                mult_matriz(m1, n1, a, m2, n2, b);
+                break;
+            case '/':
+                div_matriz(m1, n1, a, m2, n2, b);
+                break;
+            case 't':
+              traspuesta(m1, n1, a);
+                break;
+            case 'x':
+          return 0;
+            default:
+                printf("no es una opción válida\n");
+        }
+    }
+
+    return 0;
 }
